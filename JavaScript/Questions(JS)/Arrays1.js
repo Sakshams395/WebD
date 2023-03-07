2902fd99a37a44b542b27235718883c8c3eb3d2e
Saksham Sharma <sakshams395@gmail.com>
2023-03-07 10:53:48 +0530

    started arrays

---
+++ JavaScript/Questions(JS)/Arrays1.js
@@ -0,0 +1,12 @@
+concat 
+length
+
+//splice instead of slice
+
+//both works with end of the array
+Array.push('black');
+Array.pop('white');
+
+//front 
+Array.unshift('black');
+Array.shift('white');
