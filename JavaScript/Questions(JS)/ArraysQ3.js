134faf9fa1a92f575e6e2d013004b6bbe4fc34da
Saksham Sharma <sakshams395@gmail.com>
2023-03-13 15:04:39 +0530

    did a coding question

--- JavaScript/Questions(JS)/ArraysQ3.js
+++ JavaScript/Questions(JS)/ArraysQ3.js
@@ -2,3 +2,25 @@
 // insert dashes (-) between each two even numbers. For example if you 
 // accept 025468 the output should be 0-254-6-8.
 
+var number = prompt("Enter a number");
+
+document.write(number+"<br>");
+var x = [];
+
+for(var i=0; i<number.length; i++)
+{
+    var num1 = parseInt(number[i]);
+    var num2 = parseInt(number[i+1]);
+    if(num1%2==0 && num2%2==0)
+    {
+        x.push(num1+"-");
+    }
+    else
+    {
+        x.push(num1);
+    }
+}
+x.forEach
+(
+    i=>{document.write(i);}
+);
